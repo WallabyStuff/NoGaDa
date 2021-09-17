4bb680aedc70ed6309415cb4c299045b4020011b
Avocado34 <avocado34.131@gmail.com>
2021-09-17 15:02:48 +0900

    Initial Commit

---
+++ NoGaDa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoGaDa
+//
+//  Created by 이승기 on 2021/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
