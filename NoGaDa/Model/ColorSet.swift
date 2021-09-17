91fcab07cb7a62ce621fb8ad02c7b0d6422d5160
Avocado34 <avocado34.131@gmail.com>
2021-09-17 16:14:28 +0900

    ColorSet 🎨
    <New>
    ColorSet added

---
+++ NoGaDa/Model/ColorSet.swift
@@ -0,0 +1,29 @@
+//
+//  ColorSet.swift
+//  NoGaDa
+//
+//  Created by 이승기 on 2021/09/17.
+//
+
+import UIKit
+
+class ColorSet {
+    static let accentColor                      = UIColor(named: "AccentColor")!
+    static let accentSubColor                   = UIColor(named: "AccentSubColor")!
+    static let backgroundColor                  = UIColor(named: "BackgroundColor")!
+    static let buttonTextColor                  = UIColor(named: "ButtonTextColor")!
+    static let chartCellBackgroundColor         = UIColor(named: "ChartCellBackgroundColor")!
+    static let exitButtonBackgroundColor        = UIColor(named: "ExitButtonBackgroundColor")!
+    static let exitButtonForegroundColor        = UIColor(named: "ExitButtonForegroundColor")!
+    static let pointColor                       = UIColor(named: "PointColor")!
+    static let searchBarBackgroundColor         = UIColor(named: "serachBarBackgroundColor")!
+    static let searchBarPlaceholderColor        = UIColor(named: "SearchBarPlaceholderColor")!
+    static let searchButtonColor                = UIColor(named: "SearchButtonColor")!
+    static let searchResultCellBackgroundColor  = UIColor(named: "SearchResultCellBackgroundColor")!
+    static let segmentedControlBackgroundColor  = UIColor(named: "SegmentedControlBackgroundColor")!
+    static let segmentedControlForegroundColor  = UIColor(named: "SegmentedControlForegroundColor")!
+    static let segmentedControlTextColor        = UIColor(named: "SegmentedControlTextColor")!
+    static let textColor                        = UIColor(named: "TextColor")!
+    static let textfieldBackgroundColor         = UIColor(named: "TextfieldBackgroundColor")!
+    static let textfieldPlaceholderColor        = UIColor(named: "TextfieldPlaceholderColor")!
+}
