25656e0da042bd34157a8e1f0d0ddb7364e2fa46
SunneyG <avocado34.131@gmail.com>
2022-09-19 14:08:45 +0900

    Refactor: Add metric in MainViewController
    - Add floating transformer
    
    Ref: #157

---
+++ NoGaDa/Sources/Utils/Number+FloatType.swift
@@ -0,0 +1,20 @@
+//
+//  Number+FloatType.swift
+//  NoGaDa
+//
+//  Created by 이승기 on 2022/09/19.
+//
+
+import Foundation
+
+public extension IntegerLiteralType {
+  var f: CGFloat {
+    return CGFloat(self)
+  }
+}
+
+public extension FloatLiteralType {
+  var f: CGFloat {
+    return CGFloat(self)
+  }
+}
